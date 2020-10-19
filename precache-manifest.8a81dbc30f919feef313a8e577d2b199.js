03c6088972d902ae24695778647d8c0e138562c4
PhillipeAlves <phillipe.alves@gmail.com>
2020-10-19 15:14:56 +1100

    Updates

---
+++ precache-manifest.8a81dbc30f919feef313a8e577d2b199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f3567428ede1eabe90f96a7758b08a",
+    "url": "/sky/index.html"
+  },
+  {
+    "revision": "0829375cb2e170991db9",
+    "url": "/sky/static/css/main.f96375d6.chunk.css"
+  },
+  {
+    "revision": "8f72a7b435d920f7bd0b",
+    "url": "/sky/static/js/2.c0457bbc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/sky/static/js/2.c0457bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0829375cb2e170991db9",
+    "url": "/sky/static/js/main.44c25e6f.chunk.js"
+  },
+  {
+    "revision": "ed09fe7366b08ff1563a",
+    "url": "/sky/static/js/runtime-main.9b3d34a7.js"
+  }
+]);
